c529b30c8780907a33a8e2bf880ec33427721baa
Brice CARBOU <carbou@esterel-technologies.com>
2018-12-24 12:04:15 +0100

    amelioration vu des trads

--- resources/views/trads.blade.php
+++ resources/views/trads.blade.php
@@ -1,7 +1,95 @@
 @extends('layout')
 
 @section('contain')
+<style>
+        ul {
+            list-style-type: none;
+        }
 
+        li {
+            display: inline-block;
+        }
+
+        label {
+            border: 0px solid #fff;
+            padding: 0px;
+            display: block;
+            position: relative;
+            margin: -5px;
+            cursor: pointer;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+        /* 
+        Generic Styling, for Desktops/Laptops 
+        */
+        table { 
+            width: 100%; 
+            border-collapse: collapse; 
+        }
+        /* Zebra striping */
+        tr:nth-of-type(odd) { 
+            /*background: #eee; */
+        }
+        th { 
+            /*background: #333;*/ 
+            color: white; 
+            font-weight: bold; 
+        }
+        td, th { 
+            padding: 6px; 
+            border: 1px solid #ccc; 
+            text-align: left; 
+        }
+
+
+        /* 
+        Max width before this PARTICULAR table gets nasty
+        This query will take effect for any screen smaller than 760px
+        and also iPads specifically.
+        */
+        @media 
+        only screen and (max-width: 760px),
+        (min-device-width: 768px) and (max-device-width: 1024px)  {
+
+            /* Force table to not be like tables anymore */
+            table, thead, tbody, th, td, tr { 
+                display: block; 
+            }
+            
+            /* Hide table headers (but not display: none;, for accessibility) */
+            thead tr { 
+                position: absolute;
+                top: -9999px;
+                left: -9999px;
+            }
+            
+            
+            td { 
+                /* Behave  like a "row" */
+                border: none;
+                border-bottom: 1px solid #eee; 
+                position: relative;
+                padding-left: 50%; 
+            }
+            
+            td:before { 
+                /* Now like a table header */
+                position: absolute;
+                /* Top/left values mimic padding */
+                top: 6px;
+                left: 6px;
+                width: 45%; 
+                padding-right: 10px; 
+                white-space: nowrap;
+            }   
+        }
+    </style>
+    
     @auth
         <div class="section">
             <h1 class="title is-1">List of the Trads</h1>
