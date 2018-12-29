78aed9fba920d2f5fc0fee29ca72b7d3e8f2a816
Brice CARBOU <carbou@esterel-technologies.com>
2018-12-29 23:34:54 +0100

    remove a )

--- app/Trader.php
+++ app/Trader.php
@@ -126,7 +126,7 @@ class Trader extends Model implements Authenticatable
         elseif (!($discord_emit->isEmpty())) 
         {
             $id_emit = $discord_emit[0]->discord_id;
-            $msg = " ENTER YOUR Discord ID!!!!  ==> $dest->nick , I have $cardNameDest for you vs $cardNameEmit. Please contact me <@$id_emit> ($emit->nick))";
+            $msg = " ENTER YOUR Discord ID!!!!  ==> $dest->nick , I have $cardNameDest for you vs $cardNameEmit. Please contact me <@$id_emit> ($emit->nick)";
 
             (new \AG\DiscordMsg(
                 $msg, // message
