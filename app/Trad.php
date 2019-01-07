b609ccf338173302201c9a17fc84df8ad9d83657
Brice CARBOU <carbou@esterel-technologies.com>
2019-01-07 15:39:11 +0100

    upd level card

--- app/Trad.php
+++ app/Trad.php
@@ -77,13 +77,13 @@ class Trad extends Model
 				if (($cardTrader[3] === "Common") and (($cardTrader[2] >= '250') or ($cardTrader[4] > '12'))) {
 					$cardsToTrade[] = \App\Card::find($cardTrader[0]);
 				}
-				if (($cardTrader[3] === "Rare") and (($cardTrader[2] >= '50') or ($cardTrader[4] > '12'))) {
+				if (($cardTrader[3] === "Rare") and (($cardTrader[2] >= '50') or ($cardTrader[4] > '10'))) {
 					$cardsToTrade[] = \App\Card::find($cardTrader[0]);
 				}
-				if (($cardTrader[3] === "Epic") and (($cardTrader[2] >= '10')or ($cardTrader[4] > '12'))) {
+				if (($cardTrader[3] === "Epic") and (($cardTrader[2] >= '10')or ($cardTrader[4] > '7'))) {
 					$cardsToTrade[] = \App\Card::find($cardTrader[0]);
 				}
-				if( ($cardTrader[3] === "Legendary") AND (!($cardTrader[2] === false)) AND (((($cardTrader[2] >= '1') AND ($cardTrader[4] > '9')) OR ($cardTrader[2] >= '2') AND ($cardTrader[4] > '8')) OR ($cardTrader[4] > '12'))) {
+				if( ($cardTrader[3] === "Legendary") AND (!($cardTrader[2] === false)) AND (((($cardTrader[2] >= '1') AND ($cardTrader[4] > '0')) OR ($cardTrader[2] >= '2') AND ($cardTrader[4] > '1')) OR ($cardTrader[4] > '4'))) {
 					$cardsToTrade[] = \App\Card::find($cardTrader[0]);
 				}
 			}
