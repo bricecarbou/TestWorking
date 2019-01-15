1a0831b18bf62ff998c521bc30613b9773d9a878
Brice CARBOU <carbou@esterel-technologies.com>
2019-01-15 16:59:45 +0100

    up

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
          $schedule->call(function(){
-            \App\Trad::updateAll();
+            \App\Trad::updateAllnoAuth();
          })
          ->hourly();
     }
--- app/Trad.php
+++ app/Trad.php
@@ -205,4 +205,91 @@ class Trad extends Model
 			}
 		}
 	}
+
+	public static function updateAllnoAuth()
+	{
+
+		/* Delete the old trad */
+		\App\Trad::deleteOldTradsnoAuth();
+				
+		/* update of the trads */
+		$trads = \App\Trad::all();
+
+		foreach ($trads as $trad) 
+		{
+			
+			$trader = \App\Trader::find($trad->trader_id);
+			$cardsTrader = \App\Trader::RecoverTraderCards($trader->cr_key);
+
+			if (! ($cardsTrader === "error")) 
+			{
+				$cardsToTrade = array();
+
+				foreach ($cardsTrader as $cardTrader) 
+				{
+					if (($cardTrader[3] === "Common") and (($cardTrader[2] >= '250') or ($cardTrader[4] > '12'))) {
+						$cardsToTrade[] = \App\Card::find($cardTrader[0]);
+					}
+					if (($cardTrader[3] === "Rare") and (($cardTrader[2] >= '50') or ($cardTrader[4] > '10'))) {
+						$cardsToTrade[] = \App\Card::find($cardTrader[0]);
+					}
+					if (($cardTrader[3] === "Epic") and (($cardTrader[2] >= '10')or ($cardTrader[4] > '7'))) {
+						$cardsToTrade[] = \App\Card::find($cardTrader[0]);
+					}
+					if( ($cardTrader[3] === "Legendary") AND (!($cardTrader[2] === false)) AND (((($cardTrader[2] >= '1') AND ($cardTrader[4] > '1')) OR ($cardTrader[2] >= '2') AND ($cardTrader[4] > '0')) OR ($cardTrader[4] > '4'))) {
+						$cardsToTrade[] = \App\Card::find($cardTrader[0]);
+					}
+				}
+
+				foreach ($trad->cards as $card) 
+				{
+					$keep = false;
+					$card_id = $card->id;
+
+					foreach ($cardsToTrade as $cardToTrade) 
+					{
+						if(!($cardToTrade === null))
+						{
+							if ($card_id === $cardToTrade->id) 
+							{
+								$keep = true;
+								break;
+							}
+						}
+					}
+
+					if($keep === false)
+					{
+						$trad->cards()->detach($card_id);
+					}
+				}
+
+				if ($trad->cards->isEmpty())
+				{
+					Trad::find($trad->id)->delete();
+				}
+			}
+		}
+	}
+
+	public static function deleteOldTradsnoAuth()
+	{
+
+		/* update of the old trads */
+		$trads = \App\Trad::all();
+		$today = time();
+
+		//20 jours en seconde
+		$limit_date = 1296000;
+
+		foreach ($trads as $trad) 
+		{
+			if ((strtotime($trad->created_at) + $limit_date) < $today)
+			{
+				$trad->delete();
+			}
+		}
+	}
+
+
 }
