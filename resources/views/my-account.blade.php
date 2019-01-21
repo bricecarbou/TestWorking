ec2af2d8c320faee478d5b95888e9e903d1a0b76
Brice CARBOU <carbou@esterel-technologies.com>
2019-01-21 12:16:07 +0100

    debug

--- resources/views/my-account.blade.php
+++ resources/views/my-account.blade.php
@@ -18,9 +18,7 @@
         <br />
         <form class="section" action="/modify-mailling" method="post">
         {{ csrf_field() }}
-        {{dd(auth()->user()->mailling)}}
-        
-            @if (auth()->user()->mailling === 'true')
+            @if (auth()->user()->mailling === 1)
                     <input type="checkbox" name="mailling" checked>Do you want receive mail when a trade is possible? 
             @else
                     <input type="checkbox" name="mailling">Do you want receive mail when a trade is possible? 
