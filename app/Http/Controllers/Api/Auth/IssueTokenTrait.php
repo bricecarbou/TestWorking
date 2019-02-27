735f4c8d127b6bfbd9af7b1fc5dc2ee48e641fee
Brice CARBOU <carbou@esterel-technologies.com>
2019-02-27 18:03:34 +0100

    up

--- app/Http/Controllers/Api/Auth/IssueTokenTrait.php
+++ app/Http/Controllers/Api/Auth/IssueTokenTrait.php
@@ -3,24 +3,21 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use Illuminate\Http\Request;
-use Laravel\Passport\Client;
 use Illuminate\Support\Facades\Route;
 
 trait IssueTokenTrait{
 
     function IssueToken(Request $request, $grantType, $scope ="*"){
 
-        $client = Client::find(2);
-        
         $params = [
             'grant_type' => $grantType,
-            'client_id' => $client->id,
-            'client_secret' => $client->secret,
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
             'username' => $request->username ?: $request->nick,
             'scope' => $scope
         ];
      
-
+        
         $request->request->add($params);
 
         $proxy = Request::create('oauth/token', 'POST');
