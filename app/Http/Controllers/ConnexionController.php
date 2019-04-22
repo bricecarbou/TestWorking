75ac5a7a24dbe8637dc4d016abcaa19b8b9b71f7
Brice CARBOU <carbou@esterel-technologies.com>
2019-04-22 21:43:06 +0200

    up connexion check

--- app/Http/Controllers/ConnexionController.php
+++ app/Http/Controllers/ConnexionController.php
@@ -26,6 +26,38 @@ class ConnexionController extends Controller
 
         if ($result) {
             flash("Your are connected")->success();
+
+            $trader = auth()->user();
+
+            if ($trader->nick != "admin") {
+                // check the clan
+                $a = \App\Trader::RecoverTraderClan($trader->cr_key);
+
+                if ($a == "error") {
+                    flash("Error in your CR key")->error();
+        
+                    return redirect('/my-trads');
+                }
+        
+                //trouver le clan
+                $clan = \App\Clan::where('name', $a)->get()->first();
+                
+                if ($clan == null) {
+                    $crTrad = \App\Clan::where('name', 'CR Trad')->get()->first();
+                    if ($crTrad == null) {
+                        flash("Error in your CR key")->error();
+        
+                        return redirect('/my-trads');
+                    }
+                    flash("Your clan doesn't exist, you are automatically register to <CR Trad> group")->warning();
+                    $trader->clan_id = $crTrad->id;
+                } else {
+                    $trader->clan_id = $clan->id;
+                }
+        
+                $trader->save();
+            }
+
             return redirect('/my-trads');
         }
 
